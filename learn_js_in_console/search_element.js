5925663f560f230e6471a99939dc9cd0dce27fe6
Lera Garkanova <lera@MacBook-Pro-Lera.local>
2020-02-15 12:30:44 +0200

    marafon 2.0

---
+++ learn_js_in_console/search_element.js
@@ -0,0 +1,8 @@
+// // DOM-методом document.getElementById, 
+// // чтобы найти элемент "main-heading"
+
+// var heading_element = document.getElementById("main-heading")
+
+// // innerHTML мы можем узнать, что за текст находится 
+// // внутри элемента, или заменить этот текст:
+// heading_element.innerHTML;
