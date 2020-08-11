a343622b18b85fb3f060806e2f775b8f6424d908
Lera Garkanova <lera@MacBook-Pro-Lera.local>
2020-08-11 08:17:20 +0300

    learn js

---
+++ web basics/w3schools_js/list.js
@@ -0,0 +1,26 @@
+
+document.getElementById("link").innerHTML =
+// document.body.innerHTML = one;
+"<table id='table_link'><tr><td id='file'> <ol>" + 
+	"<li><a href='learn_js.html'>start JS</a> </li> " +
+	"<li><a href='array.html'>Массивы JS</a> </li> " +
+	"<li><a href='date.html'>Date JS</a> </li> " +
+	"<li><a href='math.html'>Math JS</a> </li> " +
+	"<li><a href='Conditional.html'>Условия JS</a> </li> " +
+	"<li><a href='cycle.html'>Циклы JS</a> </li> " +
+	"<li><a href='conversion_types.html'>Преобразование типов JS</a> </li> " +
+	"<li><a href='bitwise.html'>Побитовые операции JS</a> </li> " +
+	"<li><a href='js_errors.html'>Ошибки JS - бросить и попытаться поймать</a> </li> " +
+	"<li><a href='scope.html'>Область JS. JS Hoisting</a> </li> " +
+"</ol> </td> <td id='file' ><ol>" +
+	"<li><a href='strict.html'>Ключевое слово this. Let</a> </li>" +
+	"<li><a href='const.html'>Const, Функция стрелки, Debugging</a> </li>" +
+	"<li><a href='style_js.html'>Cтиль JS и правила кодирования</a> </li>" +
+	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+"</ol>	</td> </tr></table>";
