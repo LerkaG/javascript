cb0e15c58f73067df1af5ff9b04f6d1c53b5c5a6
Lera Garkanova <lera@MacBook-Pro-Lera.local>
2020-09-22 08:01:49 +0300

    new

--- web basics/w3schools_js/list.js
+++ web basics/w3schools_js/list.js
@@ -2,25 +2,25 @@
 document.getElementById("link").innerHTML =
 // document.body.innerHTML = one;
 "<table id='table_link'><tr><td id='file'> <ol>" + 
-	"<li><a href='learn_js.html'>start JS</a> </li> " +
-	"<li><a href='array.html'>Массивы JS</a> </li> " +
-	"<li><a href='date.html'>Date JS</a> </li> " +
-	"<li><a href='math.html'>Math JS</a> </li> " +
-	"<li><a href='Conditional.html'>Условия JS</a> </li> " +
-	"<li><a href='cycle.html'>Циклы JS</a> </li> " +
-	"<li><a href='conversion_types.html'>Преобразование типов JS</a> </li> " +
-	"<li><a href='bitwise.html'>Побитовые операции JS</a> </li> " +
-	"<li><a href='js_errors.html'>Ошибки JS - бросить и попытаться поймать</a> </li> " +
-	"<li><a href='scope.html'>Область JS. JS Hoisting</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='learn_js.html' >start JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='array.html'>Массивы JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='date.html'>Date JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='math.html'>Math JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='Conditional.html'>Условия JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='cycle.html'>Циклы JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='conversion_types.html'>Преобразование типов JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='bitwise.html'>Побитовые операции JS</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='js_errors.html'>Ошибки JS - бросить и попытаться поймать</a> </li> " +
+	"<li><a rel='noreferrer noopener' target='_blank' href='scope.html'>Область JS. JS Hoisting</a> </li> " +
 "</ol> </td> <td id='file' ><ol>" +
-	"<li><a href='strict.html'>Ключевое слово this. Let</a> </li>" +
-	"<li><a href='const.html'>Const, Функция стрелки, Debugging</a> </li>" +
-	"<li><a href='style_js.html'>Cтиль JS и правила кодирования</a> </li>" +
-	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
-	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
-	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
-	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
-	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
-	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
-	"<li><a href='.html'>Lorem ipsum dolor</a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='strict.html'>Ключевое слово this. Let</a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='const.html'>Const, Функция стрелки, Debugging</a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='style_js.html'>Cтиль кодирования. Практики. Частые ошибки </a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='performance.html'>Производительность JS. ES5</a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='ECMAScript6.html'>ES6. JS JSON</a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='js_forms.html'>JS Forms</a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='object_js.html'>Object, Свойства объекта</a> </li>" +
+	"<li><a rel='noreferrer noopener' href='HTML_JS_DOM.html'>HTML JS DOM</a> </li>" +
+	"<li><a rel='noreferrer noopener' href='JS_Browser_BOM.html'>JS Browser BOM</a> </li>" +
+	"<li><a rel='noreferrer noopener' target='_blank' href='.html'>Lorem ipsum dolor</a> </li>" +
 "</ol>	</td> </tr></table>";
